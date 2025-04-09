35e00a4b11d44ed3e467f46947810c276a053127
manbarmohamed <manbar.mohammed@gmail.com>
2025-04-09 01:06:37 +0100

    Project scaffolding

---
+++ src/test/java/com/api/marketbridge/MarketbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.marketbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketbridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
