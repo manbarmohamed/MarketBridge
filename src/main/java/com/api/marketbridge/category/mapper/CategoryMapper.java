3a7f832279830338c9ce81d878828aaca0ecf067
manbarmohamed <manbar.mohammed@gmail.com>
2025-04-11 00:32:24 +0100

    Add CategoryMapper for mapping between Category entity and DTOs

---
+++ src/main/java/com/api/marketbridge/category/mapper/CategoryMapper.java
@@ -0,0 +1,29 @@
+package com.api.marketbridge.category.mapper;
+
+import com.api.marketbridge.category.dto.CategoryRequest;
+import com.api.marketbridge.category.dto.CategoryResponse;
+import com.api.marketbridge.category.entity.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper()
+public interface CategoryMapper {
+    
+    CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
+    
+    // Entity to DTO mapping
+    CategoryResponse toResponse(Category category);
+    
+    // Convert a list of entities to a list of DTOs
+    List<CategoryResponse> toResponseList(List<Category> categories);
+    
+    // Request DTO to Entity mapping (for creating new categories)
+    Category toEntity(CategoryRequest categoryRequest);
+    
+    // Update an existing entity from a request DTO
+    void updateEntityFromRequest(CategoryRequest categoryRequest, @MappingTarget Category category);
+}
