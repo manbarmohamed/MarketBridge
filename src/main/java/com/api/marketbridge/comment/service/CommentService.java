365b294c58884c0bfd1c14d88a864b15839d0efb
manbarmohamed <manbar.mohammed@gmail.com>
2025-04-16 00:34:34 +0100

    Implement comment management methods in CommentService interface

--- src/main/java/com/api/marketbridge/comment/service/CommentService.java
+++ src/main/java/com/api/marketbridge/comment/service/CommentService.java
@@ -1,4 +1,13 @@
 package com.api.marketbridge.comment.service;
 
+import com.api.marketbridge.comment.dto.CommentRequest;
+import com.api.marketbridge.comment.dto.CommentResponse;
+
+import java.util.List;
+
 public interface CommentService {
+    CommentResponse addComment(CommentRequest request);
+    CommentResponse updateComment(Long id, CommentRequest request);
+    void deleteComment(Long id);
+    List<CommentResponse> getCommentsByProduct(Long productId);
 }
