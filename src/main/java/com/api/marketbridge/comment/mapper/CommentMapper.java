55b519fbb71e0a62ef097cffbc6e9000f24def15
manbarmohamed <manbar.mohammed@gmail.com>
2025-04-16 01:09:41 +0100

    Implement CommentMapper and extend CommentRepository with JpaRepository

---
+++ src/main/java/com/api/marketbridge/comment/mapper/CommentMapper.java
@@ -0,0 +1,17 @@
+package com.api.marketbridge.comment.mapper;
+
+import com.api.marketbridge.comment.dto.CommentRequest;
+import com.api.marketbridge.comment.dto.CommentResponse;
+import com.api.marketbridge.comment.entity.Comment;
+import org.mapstruct.*;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
+public interface CommentMapper {
+    Comment toEntity(CommentRequest commentRequest);
+
+    CommentRequest toDto(Comment comment);
+    CommentResponse toResponse(Comment comment);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Comment partialUpdate(CommentRequest commentRequest, @MappingTarget Comment comment);
+}
--- src/main/java/com/api/marketbridge/comment/repository/CommentRepository.java
+++ src/main/java/com/api/marketbridge/comment/repository/CommentRepository.java
@@ -1,4 +1,7 @@
 package com.api.marketbridge.comment.repository;
 
-public interface CommentRepository {
+import com.api.marketbridge.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
