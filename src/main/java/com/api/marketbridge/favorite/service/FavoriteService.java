ca6bfd214db3859c3e21b55c794da81fe55a67ef
manbarmohamed <manbar.mohammed@gmail.com>
2025-04-17 16:43:07 +0100

    Implement favorite management functionality in FavoriteService and FavoriteServiceImpl

--- src/main/java/com/api/marketbridge/favorite/service/FavoriteService.java
+++ src/main/java/com/api/marketbridge/favorite/service/FavoriteService.java
@@ -1,4 +1,12 @@
 package com.api.marketbridge.favorite.service;
 
+import com.api.marketbridge.favorite.dto.FavoriteRequest;
+import com.api.marketbridge.favorite.dto.FavoriteResponse;
+
+import java.util.List;
+
 public interface FavoriteService {
+    FavoriteResponse addToFavorites(FavoriteRequest request);
+    void removeFavorite(Long favoriteId);
+    List<FavoriteResponse> getFavoritesByBuyer(Long buyerId);
 }
--- src/main/java/com/api/marketbridge/favorite/service/impl/FavoriteServiceImpl.java
+++ src/main/java/com/api/marketbridge/favorite/service/impl/FavoriteServiceImpl.java
@@ -1,4 +1,26 @@
 package com.api.marketbridge.favorite.service.impl;
 
-public class FavoriteServiceImpl {
+import com.api.marketbridge.favorite.dto.FavoriteRequest;
+import com.api.marketbridge.favorite.dto.FavoriteResponse;
+import com.api.marketbridge.favorite.service.FavoriteService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FavoriteServiceImpl implements FavoriteService {
+    @Override
+    public FavoriteResponse addToFavorites(FavoriteRequest request) {
+        return null;
+    }
+
+    @Override
+    public void removeFavorite(Long favoriteId) {
+
+    }
+
+    @Override
+    public List<FavoriteResponse> getFavoritesByBuyer(Long buyerId) {
+        return List.of();
+    }
 }
